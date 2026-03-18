807a5d545f078c15443cbd802d684298df9c99ec
Rauf Aghazada <raufaghazade9@gmail.com>
2026-03-18 16:18:39 +0400

    All steps for Milestone 2

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=100)
+    type=models.CharField(max_length=10,choices=[('income','Income'),('expense','Expense')])
+    color=models.CharField(max_length=7, default='#000000')
+    icon=models.CharField(max_length=50,blank=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        verbose_name_plural='Categories'
+
+    def __str__(self):
+        return f"{self.name} ({self.type})"
--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from categories.models import Category
 
-# Create your models here.
+class Transaction(models.Model):
+    TYPES = [('income','Income'),('expense','Expense')]
+    title = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=10, choices=TYPES)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    date = models.DateField()
+    notes = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date', '-created_at']
+
+    def __str__(self):
+        return f"{self.name} ({self.type})"
